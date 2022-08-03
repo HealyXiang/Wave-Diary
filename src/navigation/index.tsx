c8c9a0a3436fb89f948d1a2086a7c4f63fb3b034
xiangli <healy@192.168.0.104>
2022-08-03 09:22:45 +0800

    feat: add tab navigation

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,28 +1,66 @@
-// In App.js in a new project
-
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+// You can import Ionicons from @expo/vector-icons/Ionicons if you use Expo or
+// react-native-vector-icons/Ionicons otherwise.
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// (...)
 
 function HomeScreen() {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home!</Text>
     </View>
   );
 }
 
-const Stack = createNativeStackNavigator();
-
-function App() {
+function SettingsScreen() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Settings!</Text>
+    </View>
   );
 }
 
-export default App;
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route, navigation}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+            console.log('route:', route);
+            console.log('focused：', focused);
+            console.log('color:', color);
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return (
+              <Ionicons name={iconName as string} size={size} color={color} />
+            );
+          },
+          tabBarActiveBackgroundColor: 'blue',
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{tabBarBadge: 3}}
+        />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
